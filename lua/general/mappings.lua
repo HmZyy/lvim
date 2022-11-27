050c01e4dcf05b7721fc2d6ce78279c2c7d39b4f
Hamza ZOGHMAR <hamza@tilak.io>
2022-11-27 20:14:03 +0100

    added neoclip plugin

--- lua/general/mappings.lua
+++ lua/general/mappings.lua
@@ -34,21 +34,112 @@ H.config = function()
   --   },
   -- }
 
-  lvim.builtin.which_key.mappings["t"] = {
-    name = "+Trouble",
-    r = { "<cmd>Trouble lsp_references<cr>", "References" },
-    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-    d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-    w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+  lvim.builtin.which_key.setup.icons.separator = ""
+
+  lvim.builtin.which_key.mappings = {
+    ["?"] = { "<cmd>Cheat<cr>", "Cheat" },
+    [" "] = { "<cmd>Telescope buffers<cr>", "Select Buffer" },
+    [";"] = { "<cmd>Alpha<CR>", "Dashboard" },
+    ["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle" },
+
+    w = { "<cmd>w!<CR>", "Save" },
+    q = { "<cmd>lua require('lvim.utils.functions').smart_quit()<CR>", "Quit" },
+    c = { "<cmd>BufferKill<CR>", "Close Buffer" },
+    f = { require("lvim.core.telescope.custom-finders").find_project_files, "Find File" },
+    t = { "<cmd>Telescope live_grep<cr>", "Find Text" },
+    z = { "<cmd>ZenMode<cr>", "Zen Mode" },
+    o = { "<cmd>SymbolsOutline<cr>", "Symbole Outline" },
+    p = { "<cmd>Telescope neoclip theme=dropdown<cr>", "Neoclip" },
+    T = {
+      name = "+Trouble",
+      r = { "<cmd>Trouble lsp_references<cr>", "References" },
+      f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+      d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+      q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+      l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+      w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+    },
+    P = {
+      name = "Packer",
+      c = { "<cmd>PackerCompile<cr>", "Compile" },
+      i = { "<cmd>PackerInstall<cr>", "Install" },
+      r = { "<cmd>lua require('lvim.plugin-loader').recompile()<cr>", "Re-compile" },
+      s = { "<cmd>PackerSync<cr>", "Sync" },
+      S = { "<cmd>PackerStatus<cr>", "Status" },
+      u = { "<cmd>PackerUpdate<cr>", "Update" },
+    },
+    g = {
+      name = "Git",
+      g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
+      j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
+      k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
+      l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+      p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+      r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+      R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+      s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+      u = {
+        "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+        "Undo Stage Hunk",
+      },
+      o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+      b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+      c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+      C = {
+        "<cmd>Telescope git_bcommits<cr>",
+        "Checkout commit(for current file)",
+      },
+      d = {
+        "<cmd>Gitsigns diffthis HEAD<cr>",
+        "Git Diff",
+      },
+    },
+    l = {
+      name = "LSP",
+      a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+      d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+      w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+      f = { require("lvim.lsp.utils").format, "Format" },
+      i = { "<cmd>LspInfo<cr>", "Info" },
+      I = { "<cmd>Mason<cr>", "Mason Info" },
+      j = {
+        vim.diagnostic.goto_next,
+        "Next Diagnostic",
+      },
+      k = {
+        vim.diagnostic.goto_prev,
+        "Prev Diagnostic",
+      },
+      l = { vim.lsp.codelens.run, "CodeLens Action" },
+      q = { vim.diagnostic.setloclist, "Quickfix" },
+      r = { vim.lsp.buf.rename, "Rename" },
+      s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+      S = {
+        "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+        "Workspace Symbols",
+      },
+      e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+    },
+    s = {
+      name = "Search",
+      b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+      c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+      f = { "<cmd>Telescope find_files<cr>", "Find File" },
+      h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+      H = { "<cmd>Telescope highlights<cr>", "Find highlight groups" },
+      M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+      r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+      R = { "<cmd>Telescope registers<cr>", "Registers" },
+      t = { "<cmd>Telescope live_grep<cr>", "Text" },
+      k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+      C = { "<cmd>Telescope commands<cr>", "Commands" },
+      p = {
+        "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<cr>",
+        "Colorscheme with Preview",
+      },
+    },
   }
 
-  -- lvim.builtin.which_key.mappings["z"] = { ":Goyo<cr>", "Zen Mode" }
-  lvim.builtin.which_key.mappings["z"] = { "<cmd>ZenMode<cr>", " Zen Mode" }
-  lvim.builtin.which_key.mappings["?"] = { "<cmd>Cheat<cr>", " Cheat" }
-  lvim.builtin.which_key.mappings["o"] = { "<cmd>SymbolsOutline<cr>", " Symbole Outline" }
 
-  lvim.builtin.which_key.mappings[" "] = { "<cmd>Telescope buffers<cr>", " Select Buffer" }
 end
 return H
--- lua/general/plugins.lua
+++ lua/general/plugins.lua
@@ -42,7 +42,19 @@ H.config = function()
     },
 
     -- Colorizer
-    { 'norcalli/nvim-colorizer.lua' }
+    { 'norcalli/nvim-colorizer.lua' },
+
+    -- neoclip
+    {
+      "AckslD/nvim-neoclip.lua",
+      requires = {
+        { 'kkharji/sqlite.lua', module = 'sqlite' },
+        { 'nvim-telescope/telescope.nvim' },
+      },
+      config = function()
+        require("plugins.neoclip").config()
+      end,
+    }
 
 
 
