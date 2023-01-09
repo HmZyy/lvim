5f915c25b01d7b51b0cddc25c59af11513217258
Hamza ZOGHMAR <hamza@tilak.io>
2023-01-09 09:13:23 +0100

    non transparent window

--- lua/general/appearance.lua
+++ lua/general/appearance.lua
@@ -1,6 +1,7 @@
 local H = {}
 H.config = function()
-  lvim.colorscheme = "kanagawa"
+  -- lvim.colorscheme = "kanagawa"
+  lvim.colorscheme = "slate"
   -- to disable icons and use a minimalist setup, uncomment the following
   -- lvim.use_icons = false
   lvim.builtin.alpha.active = true
@@ -9,7 +10,7 @@ H.config = function()
   lvim.builtin.nvimtree.setup.view.side = "left"
   lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
   lvim.builtin.lualine.active = true
-  lvim.transparent_window = true
+  -- lvim.transparent_window = true
 
   vim.opt.wrap = true
   vim.g.cheat_default_window_layout = "vertical_split"
